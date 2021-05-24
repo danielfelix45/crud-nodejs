c3d9f81873071742e1d5bb80dc37be48601f82d5
Daniel Felix <felixdaniel9@gmail.com>
2021-05-24 16:54:48 -0300

    novo

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const sql = mysql.createPool({
     database: "heroku_bcfbdc9d1a1d611"
 })
 
+let port = process.env.PORT || 3000;
 app.use('/css', express.static('css'))
 app.use('/js', express.static('js'))
 app.use('/img', express.static('img'))
@@ -27,53 +28,13 @@ app.use('/img', express.static('img'))
         //res.send('essa é minha página inicial')
         //res.sendFile(__dirname+ "/index.html")
         //console.log(req.params.id)
-        res.render('index')
+        res.send('Oi')
     })
-    app.get("/inserir", function(req, res) {
-        res.render("inserir")
-    })
-    app.get("/select/:id?", function(req, res) {
-        if(!req.params.id){
-            sql.getConnection(function(err, connection){
-                connection.query("select * from user order by id asc", function(err,results,fields){
-                    res.render("select", {data: results})
-                }) 
-            })
-            
-        } else{
-            sql.getConnection(function(err, connection){
-                sql.query("select * from user where id=? order by id asc",[req.params.id], function(err,results,fields){
-                    res.render("select", {data: results})
-                })
-            })
-            
-        }
-    })
-    app.post("/controllerForm", urlencondeParser, function(req, res) {
-        sql.query("insert into user values(?,?,?)", [req.body.id,req.body.name,req.body.age])
-        res.render("controllerForm", {name: req.body.name})
-    })
-    app.get('/deletar/:id', function(req, res){
-        sql.query("delete from user where id=?", [req.params.id])
-        res.render('deletar')
-    })
-    app.get("/update/:id", function(req, res){
-        sql.query("select * from user where id=?", [req.params.id], function(err,results,fields){
-            res.render("update", {id:req.params.id, name:results[0].name, age:results[0].age})
-        })    
-    })
-    app.post("/controllerUpdate", urlencondeParser, function(req, res){
-        sql.query("update user set name=?, age=? where id=?",[req.body.name,req.body.age,req.body.id])
-        res.render("controllerUpdate");
-    })
-
-
-
-
+    
 
 
 
 // Start server
-app.listen(3000, function(req, res) {
+app.listen(port, function(req, res) {
     console.log('Servidor rodando!')
 })
