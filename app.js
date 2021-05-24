87756c9b9dc50caa61b18bfb810c98489163b619
Daniel Felix <felixdaniel9@gmail.com>
2021-05-24 17:23:11 -0300

    alteracao do banco

--- app.js
+++ app.js
@@ -28,9 +28,45 @@ app.use('/img', express.static('img'))
         //res.send('essa é minha página inicial')
         //res.sendFile(__dirname+ "/index.html")
         //console.log(req.params.id)
-        res.send('Oi')
+        res.render('index')
     })
-    
+    app.get("/inserir", function(req, res) {
+        res.render("inserir")
+    })
+    app.get("/select/:id?", function(req, res) {
+        if(!req.params.id){
+            sql.getConnection(function(err, connection){
+                connection.query("select * from user order by id asc", function(err,results,fields){
+                    res.render("select", {data: results})
+                }) 
+            })
+            
+        } else{
+            sql.getConnection(function(err, connection){
+                sql.query("select * from user where id=? order by id asc",[req.params.id], function(err,results,fields){
+                    res.render("select", {data: results})
+                })
+            })
+            
+        }
+    })
+    /*app.post("/controllerForm", urlencondeParser, function(req, res) {
+        sql.query("insert into user values(?,?,?)", [req.body.id,req.body.name,req.body.age])
+        res.render("controllerForm", {name: req.body.name})
+    })
+    app.get('/deletar/:id', function(req, res){
+        sql.query("delete from user where id=?", [req.params.id])
+        res.render('deletar')
+    })
+    app.get("/update/:id", function(req, res){
+        sql.query("select * from user where id=?", [req.params.id], function(err,results,fields){
+            res.render("update", {id:req.params.id, name:results[0].name, age:results[0].age})
+        })    
+    })
+    app.post("/controllerUpdate", urlencondeParser, function(req, res){
+        sql.query("update user set name=?, age=? where id=?",[req.body.name,req.body.age,req.body.id])
+        res.render("controllerUpdate");
+    })*/
 
 
 
